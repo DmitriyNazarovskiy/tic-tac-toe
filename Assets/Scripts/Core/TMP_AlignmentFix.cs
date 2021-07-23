7d0138159d8dacd5c595b62edad16b84a6dbc097
Dmitriy Nazarovskiy <dmitriy.nazarovskiy@litslink.com>
2021-07-23 09:46:26 +0300

    TMP alignment fix

---
+++ Assets/Scripts/Core/TMP_AlignmentFix.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Core
+{
+	/// <summary>
+	/// This fix required for exporting .unitypackage
+	/// </summary>
+	[RequireComponent(typeof(TMP_Text))]
+	public class TMP_AlignmentFix : MonoBehaviour
+	{
+		[SerializeField] private TextAlignmentOptions _alignment;
+
+		private void Awake() => GetComponent<TMP_Text>().alignment = _alignment;
+	}
+}
